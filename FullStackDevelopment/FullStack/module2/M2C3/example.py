cccb049325dd855b08814a1d631681b4e234aa6d
lopuma <lopumacp@hotmail.com>
2023-03-31 04:29:01 +0200

    31032023

---
+++ FullStackDevelopment/FullStack/module2/M2C3/example.py
@@ -0,0 +1,39 @@
+#TODO exercise1
+my_string = "hello world"
+my_number = 33
+my_list = [1, 2, 3, 4, 5]
+my_boolean = True
+
+#TODO exercise2
+three_letters = my_string[0:3]
+print(three_letters)
+
+#TODO exercise3
+first_element = my_list[0]
+print(first_element)
+
+#TODO exercise4
+my_number2 = 10
+sum = my_number + my_number2
+print(sum)
+
+
+#TODO exercise5
+last_element = my_list[-1]
+print(last_element)
+
+#TODO exercise6
+names = 'harry,alex,susie,jared,gail,conner'
+names_list = names.split(',')
+print(type(names_list), " ", names_list)
+
+#TODO exercise7
+first_letters = my_string[0]
+new_string = first_letters.upper() + my_string[1:]
+print(new_string)
+
+#TODO exercise8
+print(f"Hi, I'm Jose, I'm {my_number} years old.")
+
+#TODO exercise9
+print("hello world")
